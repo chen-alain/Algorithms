79a80f147d83aca60ec3a44b23f50275dd9430aa
ALAIN CHEN <acmyf@mail.missouri.edu>
2016-11-23 21:20:05 -0600

    Create Point.java

---
+++ Algorithms-in-A.I./Adversarial-Search/Point.java
@@ -0,0 +1,18 @@
+/*
+ * This class stores a location on the board.
+ * Alain Chen
+ * 
+ * Created on 9/27/2016
+ */
+package AdversarialGameSearch;
+
+public class Point {
+    public int x;
+    public int y;
+    public Point(int x,int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    
+}
